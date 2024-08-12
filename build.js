338eee98b15514a7e0e2d5a50dde1b83bb448cff
Hernawan Faiz Abdillah <hernawan.faiz.abdillah@gmail.com>
2024-08-12 12:47:45 +0700

    chore(build): add watch and rebuild mode

--- build.js
+++ build.js
@@ -1,37 +1,51 @@
-function build(options) {
+async function build(options) {
     options.define || (options.define = {})
 
-    return require('esbuild').build({
+    const ctx = await require('esbuild').context({
         logLevel: process.argv.includes('--watch') ? 'info' : 'warning',
-        // watch: process.argv.includes('--watch'),
-        // external: ['alpinejs'],
         ...options,
-    }).catch(() => process.exit(1))
+    });
+
+    const result = await ctx.rebuild();
+    console.log(`✨ Build '${options.outfile}' succeeded.`);
+
+    if (process.argv.includes('--watch')) {
+        // Enable watch
+        console.log(`👀 Watching '${options.outfile}'..`);
+        ctx.watch();
+    }
+
+    return ctx;
 }
 
 const pkgs = require('./package.json');
 const modules = {
-  'alpinejs': {
-    'define': {
-      'ALPINE_VERSION': `'${pkgs.dependencies['alpinejs'].replace(/[\^]/, '')}'`
+    'alpinejs': {
+        'define': {
+            'ALPINE_VERSION': `'${pkgs.dependencies['alpinejs'].replace(/[\^]/, '')}'`
+        },
+        'entryPoints': [ 'src/index.js' ],
     },
-    'entryPoints': ['src/index.js'],
-  },
-  '@vue/reactivity': {
-    'entryPoints': ['dist/reactivity.esm-browser.js'],
-  },
+    '@vue/reactivity': {
+        'entryPoints': [ 'dist/reactivity.esm-browser.js' ],
+    },
 };
 
 const isProduction = process.argv.includes('--production');
 const externals = Object.keys(modules);
 
+const promises = [];
 for (const k in modules) {
-  const module = modules[k]
-  build(Object.assign(module, {
-    format: 'esm',
-    bundle: true,
-    external: externals,
-    outfile: `mod/${k}/index${isProduction ? '.min' : ''}.js`,
-    entryPoints: module.entryPoints.map((ep) => `node_modules/${k}/${ep}`),
-  }));
+    const module = modules[k]
+    const p = build(Object.assign(module, {
+        format: 'esm',
+        bundle: true,
+        external: externals,
+        outfile: `mod/${k}/index${isProduction ? '.min' : ''}.js`,
+        entryPoints: module.entryPoints.map((ep) => `node_modules/${k}/${ep}`),
+    }));
+    promises.push(p);
 }
+
+Promise.all(promises)
+.then(() => process.exit(0))
